7d00470461b6551be933d3b18be7cde9c0e6e77b
Silviu Obreajan <obreajansilviu@yahoo.com>
2022-06-24 19:14:34 +0300

    packages updates

--- src/main/java/com/fintech/appuser/UserRole.java
+++
@@ -1,6 +0,0 @@
-package com.fintech.appuser;
-
-public enum UserRole {
-    USER,
-    ADMIN
-}
--- src/main/java/com/fintech/email/EmailSender.java
+++
@@ -1,6 +0,0 @@
-package com.fintech.email;
-
-public interface EmailSender {
-
-    void send(String recipient, String email);
-}
